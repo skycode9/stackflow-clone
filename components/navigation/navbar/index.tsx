52a47005f3ca01b3e34ee5628a40bd23410d28ed
skycode9 <skycode9005@gmail.com>
2026-05-05 16:24:52 -0600

    Add Theme component to Navbar
    - Imported Theme component into Navbar
    - Replaced placeholder text with Theme component in navigation bar

--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import Theme from "./Theme";
 
 const Navbar = () => {
   return (
@@ -17,7 +18,9 @@ const Navbar = () => {
         </p>
       </Link>
       <p>Global Serch</p>
-      <div className="flex-between gap-5">Theme</div>
+      <div className="flex-between gap-5">
+        <Theme></Theme>
+      </div>
     </nav>
   );
 };
